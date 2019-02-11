f26ab83d55989ecd09a108e33faa08dcd8de9044
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2019-02-11 14:19:05 +0800

    Created message schema

---
+++ models/Message.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var MessageSchema = new mongoose.Schema({
+  messageBody: {
+    type: String,
+    required: true
+  },
+  postedAt: Number
+})
+
+
+MessageSchema.pre('save', function (next) {
+  var Message = this
+  var id = new mongoose.mongo.ObjectId()
+  Message._id = id
+  Message.postedAt = (new Date()).getTime()
+  next()
+})
+
+var Message = mongoose.model('Message', MessageSchema)
+module.exports = Message
